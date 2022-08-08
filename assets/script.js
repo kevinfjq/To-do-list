0024b02d51c287bcc1c84d6451beb69a6f4f0b39
kevifjq <kevifjq@gmail.com>
2022-08-08 01:09:21 -0300

    Add function to add tasks and style them when checked

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+let totalTasks = 1;
+function addTask(){
+    let taskContent = document.getElementById("input").value;
+    let taskArea = document.getElementById("task-area");
+
+    let taskLabel = document.createElement("label");
+    taskLabel.setAttribute("for", "task"+totalTasks);
+    taskLabel.className = "label"
+    taskLabel.innerHTML = taskContent;
+
+    let taskHolder = document.createElement("input");
+    taskHolder.setAttribute("type", "checkbox");
+    taskHolder.setAttribute("id", "task"+totalTasks);
+    taskHolder.className = "task";
+
+    let taskItem = document.createElement("div");
+    taskItem.className = "task-item";
+
+    taskItem.append(taskHolder);
+    taskItem.append(taskLabel);
+
+    taskArea.appendChild(taskItem);
+    
+
+    totalTasks++;
+}
+
+document.getElementById("add-button").addEventListener("click", stopFormDef, false);
+
+function stopFormDef(evt){
+    evt.preventDefault();
+}
