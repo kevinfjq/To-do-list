c681e3be99ce11eec63efccd97cd5079ecbb9532
kevifjq <kevifjq@gmail.com>
2022-08-08 10:15:24 -0300

    add input validation and add confirmation to delete all function

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,34 @@
 let totalTasks = 0;
 function addTask(){
-    let taskContent = document.getElementById("input").value;
-    let taskArea = document.getElementById("task-area");
+    let taskContent = document.getElementById("input").value.trim();
+    if(taskContent){
+        let taskArea = document.getElementById("task-area");
 
-    let taskLabel = document.createElement("label");
-    taskLabel.setAttribute("for", "task"+totalTasks);
-    taskLabel.className = "label"
-    taskLabel.innerHTML = taskContent;
+        let taskLabel = document.createElement("label");
+        taskLabel.setAttribute("for", "task"+totalTasks);
+        taskLabel.className = "label"
+        taskLabel.innerHTML = taskContent;
 
-    let taskHolder = document.createElement("input");
-    taskHolder.setAttribute("type", "checkbox");
-    taskHolder.setAttribute("id", "task"+totalTasks);
-    taskHolder.className = "task";
+        let taskHolder = document.createElement("input");
+        taskHolder.setAttribute("type", "checkbox");
+        taskHolder.setAttribute("id", "task"+totalTasks);
+        taskHolder.className = "task";
 
-    let taskItem = document.createElement("div");
-    taskItem.className = "task-item";
-    taskItem.setAttribute("id", "task-container"+totalTasks);
+        let taskItem = document.createElement("div");
+        taskItem.className = "task-item";
+        taskItem.setAttribute("id", "task-container"+totalTasks);
 
-    taskItem.append(taskHolder);
-    taskItem.append(taskLabel);
+        taskItem.append(taskHolder);
+        taskItem.append(taskLabel);
 
-    taskArea.appendChild(taskItem);
-    
+        taskArea.appendChild(taskItem);
+        
 
-    totalTasks++;
+        totalTasks++;
+    }
+    else{
+        alert("Invalid! You can't create empty tasks");
+    }
 
     document.getElementById("input").value = "";
     
@@ -38,10 +43,13 @@ function stopFormDef(evt){
 
 function deleteAll(){
     let conf = confirm("Are you sure you want to delete all tasks?");
+    if(conf){
         for(let i = 0; i<totalTasks; i++){
             let temp = document.getElementById("task-container"+i);
             temp.remove();
         }
         totalTasks = 0;
+    }
+       
 }
 
